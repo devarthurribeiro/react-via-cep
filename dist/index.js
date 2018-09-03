af84d7ae5d490471ecbefbe90a9e626c65b5c769
Arthur Ribeiro <devarthurribeiro@gmail.com>
2018-09-03 14:55:58 -0300

    add examples

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ViaCep = require('./components/ViaCep');
+
+var _ViaCep2 = _interopRequireDefault(_ViaCep);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _ViaCep2.default;
